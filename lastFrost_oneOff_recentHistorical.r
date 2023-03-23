425ca342c3ccdb066bb47cebb7ccc5ebf730b4cd
atashie <51334454+atashie@users.noreply.github.com>
2023-03-23 11:17:29 -0400

    weekly updates
    weekly updates

--- lastFrost_oneOff_recentHistorical.r
+++ lastFrost_oneOff_recentHistorical.r
@@ -9,98 +9,46 @@ library(data.table)
 	# names and variables
 growYearEndDate = as.Date('2023-08-01')
 dataOrigination = c('ERA5', 'CFS', 'SEAS5')
-dataPath = 'J:\\Cai_data\\Simplot\\lastFrost\\MountainWest\\'			#	MountainWest, 			NorthernGreatPlains,			AllGreatPlains,			Midwest
-#'J:\\Cai_data\\Nuseed\\'   , NuseedSouth   #startDateClimatology = '1990-08-01'	# NuseedSouth
-storeLocations = NA		# 			'storLocs.csv' 			# either NA or a csv with store locations
-era5RecentDataName = 'SimplotMtnWst-testing-recent-era.nc'				#	SimplotMtnWst			SimplotNorthGrtPlns				SimplotAllGrtPlns,		SimplotMidwest
-era5ClimatologyDataName = 	'SimplotMtnWst-testing-climatology-era.nc'#	SimplotMtnWst			SimplotNorthGrtPlns				SimplotAllGrtPlns,		SimplotMidwest
-cfsDataName = 				'SimplotMtnWst-testing-cfs.nc'				#	SimplotMtnWst			SimplotNorthGrtPlns				SimplotAllGrtPlns,		SimplotMidwest
-seas5DataName = 			'SimplotMtnWst-testing-seas5.nc'			#	SimplotMtnWst			SimplotNorthGrtPlns				SimplotAllGrtPlns,		SimplotMidwest
-userName = 'Simplot Mountain West' 										#Simplot Mountain West,  	Simplot Northern Great Plains,	Simplot Great Plains,	Simplot Midwest
+dataPath = 'J:\\Cai_data\\Nuseed\\'
+storeLocations = 'storLocs.csv' 			
+era5RecentDataName = 'NuseedSouth-testing-recent-era.nc'	
+userName = 'NuseedSouth' 										#Simplot Mountain West,  	Simplot Northern Great Plains,	Simplot Great Plains,	Simplot Midwest, 	NuseedSouth
 startDateEra5 = '2022-08-01'
-startDateCfs = '2023-03-12'
-startDateSeas5 = '2023-02-01'
-startDateClimatology = '2002-07-01'	# All Great Plains; MountainWest
-#startDateClimatology = '2000-08-01'	# Midwest; NorthernGreatPlains
+startDateClimatology = '1990-08-01'	# NuseedSouth
 
+	# reading in data
+ncin = nc_open(paste0(dataPath, era5RecentDataName)) # [longitude,latitude,time]   (Contiguous storage) 
+ncvar_get(ncin, 'time') + as.Date(startDateEra5)
+nc_lat = ncvar_get(ncin, 'latitude')
+nc_lon = ncvar_get(ncin, 'longitude')
+nc_date = ncvar_get(ncin, 'time') + as.Date(startDateEra5)
+nc_tmin = ncvar_get(ncin, 't2m_min')
 
-cfsModels = 1:4	# number of cfs models used
-seas5Models = 1:51	# number of seas5 models used
-
-	# define the 'sortable' variables
-theseQuantiles = c(0.05, 0.25, 0.5, 0.75, 0.95)
-lastFrostThresholds = c(-2, -1, 0, 1)	# in C
-consecDaysThresholds = c(1, 2, 5)
-forecastDate = ncvar_get(nc_open(paste0(dataPath, cfsDataName)), 'lead_time')[1] + as.Date(startDateCfs) 
-	# test dates
-ncvar_get(nc_open(paste0(dataPath, cfsDataName)), 'lead_time') + as.Date(startDateCfs) 
-ncvar_get(nc_open(paste0(dataPath, era5RecentDataName)), 'time') + as.Date(startDateEra5)
-ncvar_get(nc_open(paste0(dataPath, seas5DataName)), 'lead_time') + as.Date(startDateSeas5)
-ncvar_get(nc_open(paste0(dataPath, era5ClimatologyDataName)), 'time') + as.Date(startDateClimatology)
-
-#################################################################################################
-##	historic data / climatology only needs to be run once, then data are stored for future use
-#historicOutput = f_historiclastFrost(
-#	theseQuantiles = theseQuantiles,
-#	lastFrostThresholds = lastFrostThresholds,
-#	consecDaysThresholds = consecDaysThresholds,
-#	growYearEndDate = growYearEndDate,
-#	dataPath = dataPath,
-#	dataName = era5ClimatologyDataName, 
-#	userName = userName,
-#	forecastDate = forecastDate,
-#	startDateClimatology = startDateClimatology)
+#nc_doy = yday(nc_date)
+#nc_year = year(nc_date)
 	
-#fwrite(historicOutput, paste0(dataPath, "climatologyLastFrost_21DEC2022.csv"))
-##################################################################################################
+storeLocs = fread(paste0(dataPath, storeLocations))
 
+dataOutput = data.frame(Date = nc_date)
 
-##################################################################################################
-##	forecast data, to be run every week
-projectedOutput = f_projectedlastFrost(
-	theseQuantiles = theseQuantiles,
-	lastFrostThresholds = lastFrostThresholds,
-	consecDaysThresholds = consecDaysThresholds,
-	growYearEndDate = growYearEndDate,
-	dataPath = dataPath,
-	era5RecentDataName = era5RecentDataName,
-	cfsDataName = cfsDataName,
-	seas5DataName = seas5DataName,
-	startDateEra5 = startDateEra5,
-	startDateCfs = startDateCfs,
-	startDateSeas5 = startDateSeas5,
-	cfsModels = cfsModels,
-	seas5Models = seas5Models,
-	userName = userName,
-	forecastDate = forecastDate)	
-
-
-
-historicOutput = fread(paste0(dataPath, "climatologyLastFrost_21DEC2022.csv"))
-#historicOutput$startDate = as.IDate(mdy(historicOutput$startDate))
-#historicOutput$endDate =  as.IDate(mdy(historicOutput$endDate))
-#historicOutput$forecastDate =  as.IDate(mdy(historicOutput$forecastDate))
-allOutput = rbind(projectedOutput, historicOutput)
-fwrite(allOutput, paste0(dataPath, 'lastFrost_', allOutput[1, 'User'], "_", forecastDate, '.csv'))
-summary(projectedOutput); summary(historicOutput)
-
-
-##	identifying user locations instead of heatmap, only needed for some users
-if(!is.na(storeLocations))	{
-	storeLocs = fread(paste0(dataPath, storeLocations))
-	for(i in 1:nrow(storeLocs))	{
-		closeLat = allOutput$Lat[which.min(abs(allOutput$Lat - storeLocs$Lat[i]))]
-		closeLon = allOutput$Lon[which.min(abs(allOutput$Lon - storeLocs$Lon[i]))] 
-		nrNgbr = subset(allOutput, Lat == closeLat & Lon == closeLon)
-		nrNgbr$Lat = storeLocs$Lat[i]
-		nrNgbr$Lon = storeLocs$Lon[i]
-		nrNgbr$User = paste0(userName, ' - Store Location')
-		allOutput = rbind(allOutput, nrNgbr)
-		
-	}
+for(i in 1:nrow(storeLocs))	{
+	closeLat = which.min(abs(nc_lat - storeLocs$Lat[i]))
+	closeLon = which.min(abs(nc_lon - storeLocs$Lon[i])) 
+	nrNgbr = zoo::na.fill(nc_tmin[closeLon, closeLat, ], 'extend')
+	dataOutput = cbind(dataOutput, nrNgbr)
 }
+names(dataOutput)[-1] = storeLocs$City
 
-## saving output
+dataOutput = subset(dataOutput, Date >= as.Date('2022-10-31'))
+dataOutputTF = dataOutput
+dataOutputTF[,-1] = 0
+dataOutputTF[,-1][dataOutput[,-1] <= -6] = 1
+
+fwrite(dataOutput, paste0(dataPath, 'minTemps_NuseedSouth_2022-2023.csv'))
+fwrite(dataOutputTF, paste0(dataPath, 'daysMinBelowNeg6_NuseedSouth_2022-2023.csv'))
+
+
+
 
 fwrite(allOutput, paste0(dataPath, 'lastFrost_', allOutput[1, 'User'], "_", forecastDate, '.csv'))
 summary(projectedOutput)
